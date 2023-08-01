653cdd4ea66c4e679a80cb3d66eb2d1ca305eee8
Ali Gürkan Sevilmiş <81529791+GurkanSvlms@users.noreply.github.com>
2023-08-01 15:35:24 +0300

    Initial Commit

---
+++ TranslateApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TranslateApp
+//
+//  Created by Ali Gürkan Sevilmis on 1.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
